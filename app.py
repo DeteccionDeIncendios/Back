6775d7aed595f5a44fec74a0a8d495693e37b150
Iliana <iliesbol@espol.edu.ec>
2021-01-18 20:47:58 -0500

    inicio

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, request, jsonify
+from keras.preprocessing import image
+import base64
+import json
+from io import BytesIO
+from prediction import predictSimple
+from werkzeug.utils import secure_filename
+import os
+Upload = './upload/'
+
+app = Flask(__name__)
+app.config['uploadFolder'] = Upload
+
+
+import firebase_admin
+from firebase_admin import credentials, messaging
+
+cred = credentials.Certificate("./iaproject-9cd7b-firebase-adminsdk-66qpu-3f5f300eff.json")
+firebase_admin.initialize_app(cred)
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    #se recibe el request y se guarda las fotos en la carpeta upload
+    listName = []
+    for file in request.files.listvalues():
+        photo = file[0]
+        filename = secure_filename(photo.filename)
+        photo.save(os.path.join(app.config['uploadFolder'], filename))
+        listName.append(Upload+filename)
+    #response base de ejecucion
+    response = {"fireExists": False, "files":[]}
+
+    #se realiza la predccion y se retorna una lista de los archivos o fotos que sean de clase 'fuego'
+    resp = predictSimple(listName)
+
+    #si existe alguna img de clase fuego se envia la notif a la app android
+    if(len(resp) > 0):
+        print("Se envia notificacion en caso de que exista alguna img de clase fuego")
+        #este bloque se encarga de armar la estructura de la notif y enviarla a los dispositivos que tengan el app instalada 
+        message = messaging.Message(
+            data={
+                "title":"Sistema de Detección",
+                "body": "ALERTA INCENDIO DETECTADO",
+                "color":"red"
+            },
+            topic="IA-PROJECT"
+        )
+        response = messaging.send(message)
+        print(response)
+
+    #se actualizan los valores de respuesta
+    response["fireExists"] = len(resp) > 0
+    response["files"] = resp
+
+    #se eliminan los archivos o img que se recibieron
+    for file in listName:
+        os.remove(file)
+    
+    #se retorna la respuesta json
+    return jsonify(response)
+
+                    
+if __name__ == "__main__":
+    app.run(debug = True, port=4000)
+
+                                
---
+++ prediction.py
@@ -0,0 +1,49 @@
+import numpy as np
+import os
+import re
+import pathlib
+
+from tensorflow.keras.preprocessing.image import array_to_img, img_to_array, load_img
+import zipfile
+import keras
+import matplotlib.pyplot as plt
+from skimage.transform import resize
+from io import BytesIO
+from scipy import misc
+import cv2
+import numpy as np
+from werkzeug.utils import secure_filename
+
+
+clases = ['fuego', 'no_fuego']
+batch_size = 32
+img_height = 180
+img_width = 180
+
+def predictSimple(listName):
+    THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+    my_file = os.path.join(THIS_FOLDER, 'inicial_model_update4.h5')
+    model = keras.models.load_model(my_file)
+    images = []
+    for file in listName:
+        img = plt.imread(file,0)
+        image_resized = resize(img, (36, 36),anti_aliasing=True,clip=False,preserve_range=True)
+        images.append(image_resized)
+        
+    X = np.array(images, dtype=np.uint8) #convierto de lista a numpy
+    test_X = X.astype('float32')
+    test_X = test_X / 255.
+    print(test_X)
+    predicted_classes = model.predict(test_X)
+    listResp = []
+    for i, img_tagged in enumerate(predicted_classes):
+        print("Imagen: ", listName[i])
+        classDetected = clases[img_tagged.tolist().index(max(img_tagged))];
+        print("Clase detectada: ", classDetected)
+        probability = max(img_tagged)*100
+        print("Probabilidad: "+str(probability) + "%")
+        print(listName[i], classDetected, probability)
+        if(classDetected == "fuego"):
+            listResp.append({"fileName": listName[i], "probability":str(probability)})
+        print("")
+    return listResp
